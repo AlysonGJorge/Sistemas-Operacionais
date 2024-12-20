31dd99fb592fb942ab17f3aaeb4bedb2a15f3dc0
GalafassiNAT <natanaelgalafassintg@gmail.com>
2024-12-20 00:48:38 -0300

    Nao testei ainda, mas parece fazer sentido

--- trabalho01/Monitores/mainNova.cpp
+++ trabalho01/Monitores/mainNova.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <pthread.h>
+#include <list>
 
 #define NUM_PADAWANS 5
 #define NUM_ESPECTADORES 10
@@ -28,19 +29,26 @@ class MonitorSalao {
 		pthread_cond_t condPadawan;
 		pthread_cond_t condEspectador;
 		pthread_cond_t condYoda;
+		pthread_cond_t condCortaTranca;
 
 		int totalPadawans;
+		std::list<int> padawansNoSalao;
+		std::list<int> padawansAprovados;
+		bool primeiraExecucao;
 		int countEmTeste;
 		bool salaoAberto;
-		bool todosEmSala;
+		bool testeEmAndamento;
+		bool qntdTesteSetada;
 
 	public:
 		MonitorSalao(int totalPadawans): totalPadawans(totalPadawans), countEmTeste(0), 
-		salaoAberto(false), todosEmSala(false) {
+		salaoAberto(false), primeiraExecucao(true), testeEmAndamento(false), 
+		qntdTesteSetada(false), padawansNoSalao(), padawansAprovados() {
 			pthread_mutex_init(&mutex, NULL);
 			pthread_cond_init(&condPadawan, NULL);
 			pthread_cond_init(&condEspectador, NULL);
 			pthread_cond_init(&condYoda, NULL);
+			pthread_cond_init(&condCortaTranca, NULL);
 		}
 
 		~MonitorSalao() {
@@ -48,10 +56,27 @@ class MonitorSalao {
 			pthread_cond_destroy(&condPadawan);
 			pthread_cond_destroy(&condEspectador);
 			pthread_cond_destroy(&condYoda);
+			pthread_cond_destroy(&condCortaTranca);
 		}
 
 		void setTotalPadawans(int totalPadawans) {
 			this->totalPadawans = totalPadawans;
+		}
+
+		void decrementaTotalPadawans() {
+			this->totalPadawans--;
+		}
+
+		bool estaAprovado(int id) {
+			pthread_mutex_lock(&mutex);
+			for (auto& padawan : this->padawansAprovados){
+				if (padawan == id) {
+					return true;
+				}
+			}
+
+			return false;
+			pthread_mutex_unlock(&mutex);
 		}
 
 		// Yoda
@@ -59,6 +84,10 @@ class MonitorSalao {
 		void liberarEntrada() {
 			pthread_mutex_lock(&mutex);
 			int time = rand() % 10 + 1;
+
+			while (!primeiraExecucao && this->padawansNoSalao.size() > 0) {
+				pthread_cond_wait(&condYoda, &mutex);
+			}
 
 			std::cout << "[Yoda]: Por " << time << " segundos, a entrada liberarei" << std::endl;
 			std::cout << "Yoda liberou a entrada para os próximos testes" << std::endl;
@@ -77,8 +106,66 @@ class MonitorSalao {
 
 		void iniciarTestes() {
 			pthread_mutex_lock(&mutex);
-			std::cout << "Yoda iniciou os testes" << std::endl;
-			countEmTeste = totalPadawans;
+
+			while (countEmTeste > 0) {
+				pthread_cond_wait(&condYoda, &mutex);
+			}
+
+			std::cout << "[Yoda]: Em teste, todos estão. O teste começará em breve." << std::endl;
+			testeEmAndamento = true;
+			qntdTesteSetada = false;
+
+			pthread_cond_broadcast(&condPadawan);
+
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void anunciaResultado() {
+			pthread_mutex_lock(&mutex);
+
+			while(this->countEmTeste > 0) {
+				pthread_cond_wait(&condYoda, &mutex);
+			}
+			
+			std::cout << "[Yoda]: Concluído o teste foi. Os Resultados anunciados em breve serão." << std::endl;
+			
+			for (auto& padawan : padawansNoSalao){
+				int r = rand() % 100 +1;
+				if(r >= 50) {
+					std::cout << "[Yoda]: Padawan_" << padawan << " foi aprovado" << std::endl;
+					this->padawansAprovados.push_back(padawan);
+				}
+			}
+
+			this->testeEmAndamento = false;
+			this->qntdTesteSetada = false;
+			pthread_cond_broadcast(&condPadawan);
+			
+
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void cortaTranca(){
+			pthread_mutex_lock(&mutex);
+			std::cout << "[Yoda]: A cerimônia de corte de trança começará em breve" << std::endl;
+
+			while(countEmTeste < this->padawansAprovados.size()) {
+				pthread_cond_wait(&condYoda, &mutex);
+			}
+
+			for(auto& padawan : padawansAprovados) {
+				std::cout << "[Yoda]: Padawan_" << padawan << " sua trança cortada teve. " << std::endl;
+				this->padawansAprovados.remove(padawan);
+			}
+			
+			pthread_cond_broadcast(&condCortaTranca);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void finalizaTestes() {
+			pthread_mutex_lock(&mutex);
+			std::cout << "[Yoda]: Por fim, os testes todos se encerram." << std::endl;
+			discursoYoda();
 			pthread_mutex_unlock(&mutex);
 		}
 
@@ -91,8 +178,95 @@ class MonitorSalao {
 				pthread_cond_wait(&condPadawan, &mutex); // Espera o Yoda liberar a entrada
 			}
 			std::cout << "Padawan_" << id << " entrou no salao" << std::endl;
+			cumprimentaMestresAvaliadores(id);
+			this->padawansNoSalao.push_back(id);
 			pthread_mutex_unlock(&mutex);
+		}
 
+		void saiSalaoPadawan(int id) {
+			pthread_mutex_lock(&mutex);
+
+			while (this->testeEmAndamento){
+				pthread_cond_wait(&condPadawan, &mutex);
+			}
+
+			std::cout << "Padawan_" << id << " saiu do salao" << std::endl;
+			this->padawansNoSalao.remove(id);
+
+			if (this->padawansNoSalao.size() == 0 && !this->primeiraExecucao) {
+				pthread_cond_signal(&condYoda);
+			}
+
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void aguardaTestePadawan(int id) {
+			pthread_mutex_lock(&mutex);
+			
+			if(!qntdTesteSetada) {
+				countEmTeste = totalPadawans;
+				qntdTesteSetada = true;
+			}
+
+			countEmTeste--;
+			
+			if (this->countEmTeste == 0) {
+				pthread_cond_signal(&condYoda);
+			}
+
+			while(!this->testeEmAndamento) {
+				std::cout << "Padawan_" << id << " aguardando teste" << std::endl;
+				pthread_cond_wait(&condPadawan, &mutex);
+			}
+
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void realizaTeste(int id) {
+			pthread_mutex_lock(&mutex);
+			
+			if (!this->qntdTesteSetada) {
+				countEmTeste = this->padawansNoSalao.size();
+				qntdTesteSetada = true;
+			}
+
+			countEmTeste--;
+
+			std::cout << "Padawan_" << id << " está realizando o teste." << std::endl;
+
+			if(this->countEmTeste == 0) {
+				pthread_cond_signal(&condYoda);
+			}
+			
+			while (this->testeEmAndamento) {
+				pthread_cond_wait(&condPadawan, &mutex);
+			}
+
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void aguardaCortaTranca(int id) {
+			pthread_mutex_lock(&mutex);
+			countEmTeste++;
+
+			if(countEmTeste == this->padawansAprovados.size()) {
+				pthread_cond_signal(&condYoda);
+			}
+
+			std::cout << "Padawan_" << id << " aguardando corte de tranca" << std::endl;
+			while (this->padawansAprovados.size() > 0) {
+				pthread_cond_wait(&condCortaTranca, &mutex);
+			}
+			pthread_mutex_unlock(&mutex);
+		}
+
+		void cumprimentaMestresAvaliadores(int id) {
+			std::cout << "Padawan_" << id << "cumprimentou os mestres avaliadores" << std::endl;
+
+		}
+		
+		void cumprimentaYoda(int id) {
+			std::cout << "Padawan_" << id << "decepcionadamente cumprimenta Yoda" << std::endl;
 		}
 
 };
@@ -105,13 +279,26 @@ MonitorSalao monitorSalao(NUM_PADAWANS);
 
 void *yodaThread(void *arg) {
 	std::cout << "Yoda foi criado" << std::endl;
-	discursoYoda();
+	monitorSalao.liberarEntrada();
+	monitorSalao.iniciarTestes();
+	monitorSalao.anunciaResultado();
+	monitorSalao.cortaTranca();
+	monitorSalao.finalizaTestes();
+
 	pthread_exit(NULL);
 }
 
 void *padawanThread(void *arg) {
 	int id = *(int *)arg;
-	
+	monitorSalao.entraSalaoPadawan(id);
+	monitorSalao.aguardaTestePadawan(id);
+	monitorSalao.realizaTeste(id);
+
+	if (monitorSalao.estaAprovado(id)) {
+		monitorSalao.aguardaCortaTranca(id);
+		
+	}
+
 	pthread_exit(NULL);
 }
 
@@ -126,12 +313,12 @@ int main (int argc, char **argv) {
 
 	std::cout << "Digite o numero de padawans: ";
 	std::cin >> numPadawans;
-	std::cout << "Digite o numero de espectadores: ";
-	std::cin >> numEspectadores;
+	// std::cout << "Digite o numero de espectadores: ";
+	// std::cin >> numEspectadores;
 
 	monitorSalao.setTotalPadawans(numPadawans);
 	pthread_t padawans[numPadawans];
-	pthread_t espectadores[numEspectadores];
+	// pthread_t espectadores[numEspectadores];
 
 	for(int i = 0; i < numPadawans; i++) {
 		int id = i;
@@ -139,11 +326,11 @@ int main (int argc, char **argv) {
 		pthread_create(&padawans[i], NULL, padawanThread, (void *)id);
 	}
 
-	for(int i = 0; i < numEspectadores; i++) {
-		int id = i;
-		std::cout << "Espectador_" << id << " foi criado" << std::endl;
-		pthread_create(&espectadores[i], NULL, espectadorThread, (void *)id);
-	}
+	// for(int i = 0; i < numEspectadores; i++) {
+	// 	int id = i;
+	// 	std::cout << "Espectador_" << id << " foi criado" << std::endl;
+	// 	pthread_create(&espectadores[i], NULL, espectadorThread, (void *)id);
+	// }
 
 	pthread_create(&yoda, NULL, yodaThread, NULL);
 
@@ -151,9 +338,9 @@ int main (int argc, char **argv) {
 		pthread_join(padawans[i], NULL);
 	}
 
-	for(int i = 0; i < numEspectadores; i++) {
-		pthread_join(espectadores[i], NULL);
-	}
+	// for(int i = 0; i < numEspectadores; i++) {
+	// 	pthread_join(espectadores[i], NULL);
+	// }
 
 	pthread_join(yoda, NULL);
 
